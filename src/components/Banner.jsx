8e31f1924b4cabafb80c3267fefe16d222a3a366
MaxTuron <maxx@turon.fr>
2023-04-21 14:27:40 +0200

    Update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import logoRed from "../assets/logoRed.png"
 import '../styles/banner.css';
-
 export default function Banner() {
   const URL = window.location.href
   const newUrl = URL.split(3000)
